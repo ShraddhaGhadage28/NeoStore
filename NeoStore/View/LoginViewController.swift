32911abc8ded51cd928adb6649889564aac5ae75
ShraddhaGhadage28 <shraddha.ghadage@neosoftmail.com>
2023-08-09 11:15:35 +0530

    LoginViewController and RegisterViewController UI completed

--- NeoStore/View/LoginViewController.swift
+++ NeoStore/View/LoginViewController.swift
@@ -6,14 +6,41 @@
 //
 
 import UIKit
+import Alamofire
 
-class ViewController: UIViewController {
+class LoginViewController: UIViewController {
 
+    
+    @IBOutlet weak var LoginUserName: UITextField!
+    @IBOutlet weak var LoginPassword: UITextField!
+    @IBOutlet weak var userView: UIView!
+    @IBOutlet weak var passwordView: UIView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+       viewColor()
+        placeholderColor()
     }
-
+    
+    //Mark:- placeholder color setting
+    func placeholderColor() {
+        let attributes: [NSAttributedString.Key: Any] = [
+                    .foregroundColor: UIColor.white, // Change this to your desired color
+                ]
+        let userNamePlaceholder = NSAttributedString(string: "Username", attributes: attributes)
+        let passwordPlaceholder = NSAttributedString(string: "Password", attributes: attributes)
+        
+        LoginUserName.attributedPlaceholder = userNamePlaceholder
+        LoginPassword.attributedPlaceholder = passwordPlaceholder
+    }
+    
+    //Mark:- view border color setting
+    func viewColor() {
+        userView.layer.borderWidth = 1.0
+                userView.layer.borderColor = UIColor.white.cgColor
+        passwordView.layer.borderWidth = 1.0
+                passwordView.layer.borderColor = UIColor.white.cgColor
+    }
 
 }
 
--- NeoStore/View/RegisterViewController.swift
+++ NeoStore/View/RegisterViewController.swift
@@ -9,21 +9,59 @@ import UIKit
 
 class RegisterViewController: UIViewController {
 
+   
+    @IBOutlet weak var FirstNameView: UIView!
+    @IBOutlet weak var LastNameView: UIView!
+    @IBOutlet weak var EmailView: UIView!
+    @IBOutlet weak var PasswordView: UIView!
+    @IBOutlet weak var ConfirmPasswordView: UIView!
+    
+    @IBOutlet weak var phoneView: UIView!
+    @IBOutlet weak var FirstName: UITextField!
+    @IBOutlet weak var LastName: UITextField!
+    @IBOutlet weak var Email: UITextField!
+    @IBOutlet weak var Password: UITextField!
+    @IBOutlet weak var confirmPassword: UITextField!
+    @IBOutlet weak var phoneNumber: UITextField!
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        viewColor()
+        placeholderColor()
         // Do any additional setup after loading the view.
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    func viewColor() {
+        FirstNameView.layer.borderWidth = 1.0
+                FirstNameView.layer.borderColor = UIColor.white.cgColor
+        LastNameView.layer.borderWidth = 1.0
+                LastNameView.layer.borderColor = UIColor.white.cgColor
+        EmailView.layer.borderWidth = 1.0
+                EmailView.layer.borderColor = UIColor.white.cgColor
+        PasswordView.layer.borderWidth = 1.0
+                PasswordView.layer.borderColor = UIColor.white.cgColor
+        ConfirmPasswordView.layer.borderWidth = 1.0
+                ConfirmPasswordView.layer.borderColor = UIColor.white.cgColor
+        phoneView.layer.borderWidth = 1.0
+        phoneView.layer.borderColor = UIColor.white.cgColor
     }
-    */
+
+    func placeholderColor() {
+        let attributes: [NSAttributedString.Key: Any] = [
+                    .foregroundColor: UIColor.white, // Change this to your desired color
+                ]
+        let firstNamePlaceholder = NSAttributedString(string: "First Name", attributes: attributes)
+        let lastNamePlaceholder = NSAttributedString(string: "Last Name", attributes: attributes)
+        let emailPlaceholder = NSAttributedString(string: "Email", attributes: attributes)
+        let passwordPlaceholder = NSAttributedString(string: "Password", attributes: attributes)
+        let confirmPasswordPlaceholder = NSAttributedString(string: "Confirm Password", attributes: attributes)
+        let phonePlaceholder = NSAttributedString(string: "Phone Number", attributes: attributes)
+        FirstName.attributedPlaceholder = firstNamePlaceholder
+        LastName.attributedPlaceholder = lastNamePlaceholder
+        Email.attributedPlaceholder = emailPlaceholder
+        Password.attributedPlaceholder = passwordPlaceholder
+        confirmPassword.attributedPlaceholder = confirmPasswordPlaceholder
+        phoneNumber.attributedPlaceholder = phonePlaceholder
+    }
+    
 
 }
